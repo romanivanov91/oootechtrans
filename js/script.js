c5d0119a1263db7130038683ef2490cb9597324f
Roman <romanivanov91@yandex.ru>
2022-01-20 21:08:57 +0300

    версия 2

---
+++ js/script.js
@@ -0,0 +1,12 @@
+// let containerequipments = document.querySelector(".containerequipments");
+
+// containerequipments.addEventListener('click', (event) => {
+//     if(!event.target.classList.contains('equipmentrow')) return;
+
+//     console.log(event.target.innerText);
+
+//     event.target.classList.add('active')
+//     event.target.addEventListener('click', (event) => {
+//         event.target.classList.remove('active');
+//     })
+// });
