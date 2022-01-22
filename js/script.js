6482e6aec1bac8c17ad3ba2dc15a73a6deb56d6e
Roman <romanivanov91@yandex.ru>
2022-01-22 16:54:49 +0300

    :Версия 3

--- js/script.js
+++ js/script.js
@@ -1,12 +1,22 @@
+// let body = document.querySelector("body");
 // let containerequipments = document.querySelector(".containerequipments");
 
 // containerequipments.addEventListener('click', (event) => {
 //     if(!event.target.classList.contains('equipmentrow')) return;
 
-//     console.log(event.target.innerText);
+//     let equipment = event.target;
 
-//     event.target.classList.add('active')
-//     event.target.addEventListener('click', (event) => {
-//         event.target.classList.remove('active');
-//     })
+//     console.log(equipment.innerText);
+
+//     let modalequipment = document.createElement('div');
+
+//     modalequipment.classList.add('popup');
+
+//     modalequipment.innerHTML = `<div class='popup_bg'>${equipment}</div>`;
+
+//     equipment.classList.add('popup_img');
+
+//     body.append(modalequipment);
+
+//     // equipment.classList.toggle('active');
 // });
