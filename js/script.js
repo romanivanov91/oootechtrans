e2c0afedc141e5b17731b244890a7dc020c507a6
Roman <romanivanov91@yandex.ru>
2022-01-29 10:47:35 +0300

    Версия 5

--- js/script.js
+++ js/script.js
@@ -1,25 +1,75 @@
-// let body = document.querySelector("body");
-// let containerequipments = document.querySelector(".containerequipments");
+"use strict"
 
-// containerequipments.addEventListener('click', (event) => {
-//     if(!event.target.classList.contains('equipmentrow')) return;
+/*Поиск по странице сайта. Ломает анимацию и вообще может сломать страницу. Если использовать, то надо доработать
+var lastResFind =""; // последний удачный результат
+var copy_page =""; // копия страницы в ихсодном виде
+function TrimStr(s) {
+     s = s.replace( /^\s+/g, '');
+  return s.replace( /\s+$/g, '');
+}
+function FindOnPage(inputId) {//ищет текст на странице, в параметр передается ID поля для ввода
+  var obj = window.document.getElementById("term");
+  var textToFind;
+ 
+  if (obj) {
+    textToFind = TrimStr(obj.value);//обрезаем пробелы
+  } else {
+    alert("Введенная фраза не найдена");
+    return;
+  }
+  if (textToFind == "") {
+    alert("Вы ничего не ввели");
+    return;
+  }
+ 
+  if(document.body.innerHTML.indexOf(textToFind)=="-1")
+  alert("Ничего не найдено, проверьте правильность ввода!");
+ 
+  if(copy_page.length>0)
+        document.body.innerHTML=copy_page;
+  else copy_page=document.body.innerHTML;
 
-//     let equipment = event.target;
+ 
+  document.body.innerHTML = document.body.innerHTML.replace(eval("/name="+lastResFind+"/gi")," ");//стираем предыдущие якори для скрола
+  document.body.innerHTML = document.body.innerHTML.replace(eval("/"+textToFind+"/gi"),"<a name="+textToFind+" style='background:red'>"+textToFind+"</a>"); //Заменяем найденный текст ссылками с якорем;
+  lastResFind=textToFind; // сохраняем фразу для поиска, чтобы в дальнейшем по ней стереть все ссылки
+  window.location = '#'+textToFind;//перемещаем скрол к последнему найденному совпадению
+ }
+ */
 
-//     console.log(equipment.innerText);
 
-//     let modalequipment = document.createElement('div');
+//Преобразование меню с навигацией в фиксированную при скролле
+$(window).scroll(function(){
+	if ($(window).scrollTop() > 120) {
+		$('.containernav2').addClass('containernav2-fixed');
+	}
+	else {
+		$('.containernav2').removeClass('containernav2-fixed');
+	}
+});
 
-//     modalequipment.classList.add('popup');
 
-//     modalequipment.innerHTML = `<div class='popup_bg'>${equipment}</div>`;
+//Отложенная анимация
+$(document).ready (function() {
 
-//     equipment.classList.add('popup_img');
+	let options = {threshold: [1]};
+	let observer = new IntersectionObserver(onEntry, options);
+	let elements = $('.resumeh2,.resumep');
+	elements.each ((i, el) => {
+		observer.observe (el);
+	});
+});
 
-//     body.append(modalequipment);
+function onEntry (entry) {
+	entry.forEach (change => {
+		if (change.isIntersecting) {
+			change.target.classList.add ('animClass');
+		}
+	});
+}
 
-//     // equipment.classList.toggle('active');
-// });
+
+//слайдер в разделе "Наши услуги"
 const upBtn = document.querySelector('.up-button');
 const downBtn = document.querySelector('.down-button');
 const sidebar = document.querySelector('.sidebar');
@@ -57,3 +107,12 @@ function changeSlide(direction) {
 	mainSlide.style.transform = `translateY(-${activeSlideIndex * height}px)`;
 	sidebar.style.transform = `translateY(${activeSlideIndex * height}px)`;
 }
+
+//Slick-слайдер
+$('.multiple-items').slick({
+	infinite: true,
+	slidesToShow: 3,
+	slidesToScroll: 3,
+	prevArrow: "<img src='img/prev.png' class='prev' alt='1'>",
+    nextArrow: "<img src='img/next.png' class='next' alt='2'>",
+  });
